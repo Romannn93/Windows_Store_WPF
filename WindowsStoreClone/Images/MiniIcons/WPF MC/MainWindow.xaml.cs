066ae1c4cc9cba197e79d5d21eb5e5189589e86c
Roman <papernyuk@gmail.com>
2023-02-08 20:00:09 +0400

    first

---
+++ WindowsStoreClone/Images/MiniIcons/WPF MC/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_MC.Pages;
+using WPF_MC.UserControls;
+
+namespace WPF_MC
+{
+    public partial class MainWindow : Window
+    {
+        private Main MainWindowContentPage;
+        private TopAppsWrapped MyTopAppsWrappedPage;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainWindowContentPage = new Main();
+            MainWindowContentPage.TopAppButtonClicked += MainWindowContentPage_TopAppButtonClicked;
+            MainWindowContentPage.AppClicked += MainWindowContentPage_AppClicked;
+            MyTopAppsWrappedPage = new TopAppsWrapped();
+            MyTopAppsWrappedPage.AnAppClicked += MainWindowContentPage_AppClicked;
+            MyTopAppsWrappedPage.BackButtonClicked += BackButtonClicked;
+        }
+        private void MainWindowContentPage_TopAppButtonClicked(object sender, RoutedEventArgs e)
+        {
+            MainWindowFrame.Content = MyTopAppsWrappedPage;
+        }
+
+        private void MainWindowContentPage_AppClicked(AnApp sender, RoutedEventArgs e)
+        {
+            AppDetails myAppDetails = new AppDetails(sender);
+            myAppDetails.AppClicked += MainWindowContentPage_AppClicked;
+            myAppDetails.BackButtonClicked += BackButtonClicked;
+            MainWindowFrame.Content = myAppDetails;
+        }
+        private void BackButtonClicked(object sender, RoutedEventArgs e)
+        {
+            if (MainWindowFrame.NavigationService.CanGoBack)
+                MainWindowFrame.NavigationService.GoBack();
+        }
+        private void MainWindowFrame_Loaded(object sender, RoutedEventArgs e)
+        {
+            MainWindowFrame.Content = MainWindowContentPage;
+        }
+    }
+}
